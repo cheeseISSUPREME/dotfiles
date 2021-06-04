3c4a81218411d7fed2aeaa7a6e3a6e71974108ef
Kavin valli <kavinvalli@gmail.com>
2021-06-04 20:09:43 +0530

    fix: allow user to use github provided ssh url for gcl

--- .config/fish/functions/gcl.fish
+++ .config/fish/functions/gcl.fish
@@ -1,6 +1,7 @@
 function gcl
-  set directory_name (string split -- / $argv)[-1]
-  git clone git@github.com:$argv.git && cd $directory_name
+  set directory_name_with_git (string split -- / $argv)[-1]
+  set directory_name (string split -- . $directory_name_with_git)[1]
+  git clone $argv && cd $directory_name
   echo ""
   echo "Cloned and changed to $directory_name directory"
 end
